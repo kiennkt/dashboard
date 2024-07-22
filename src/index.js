06d28b8b886f486bc7cc67c0bcf87209352fffcd
kiennkt <kimtuankien2002@gmail.com>
2024-07-22 15:39:26 +0700

    feat(change): index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
-
 import { SoftUIControllerProvider } from "context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
